02a085806f8ea1651f26d4491da0809bd9eee3d4
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-04-15 09:15:48 -0300

    criada pagina de registro de usuario

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import React from 'react';
 
 import './global.css';
 
-import Logon from './pages/Logon';
-
-// JSX (JavaScript XML)
-// Componente é uma função que retorna um HTML
+import Routes from './routes';
 
 function App() {
   return (
-    <Logon />
+    <Routes />
   );
 }
 
